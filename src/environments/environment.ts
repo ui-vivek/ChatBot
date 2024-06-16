f3e3a6491ab2f8b492ed2861d5c33da11560f609
vivek singh <cu.vivek1557@gmail.com>
2024-06-16 16:04:22 +0530

    env file

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://localhost:3000/api/v1',
+//     debugMode: true,
+// };
+export const environment = {
+    production: true,
+    apiUrl: 'https://chatbot-eiki.onrender.com/api/v1',
+    debugMode: false,
+};
